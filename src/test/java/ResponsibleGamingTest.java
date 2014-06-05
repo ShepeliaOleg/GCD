1d39517520fa547497cff2234975cf77eefff61a
Ivan Vasylchenko <ivan.vasylchenko@playtech.com>
2014-06-05 18:27:28 +0300

    SelfExclusion separated
    Signed-off-by: Ivan Vasylchenko <ivan.vasylchenko@playtech.com>

--- src/test/java/ResponsibleGamingTest.java
+++ src/test/java/ResponsibleGamingTest.java
@@ -3,25 +3,13 @@ import enums.PlayerCondition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.testng.annotations.Test;
-import pageObjects.base.AbstractPage;
-import pageObjects.header.Header;
-import pageObjects.header.LoggedInHeader;
 import pageObjects.responsibleGaming.ResponsibleGamingPage;
-import pageObjects.responsibleGaming.SelfExcludeConfirmPopup;
-import pageObjects.responsibleGaming.SelfExcludePopup;
 import springConstructors.UserData;
 import testUtils.AbstractTest;
 import utils.NavigationUtils;
 import utils.PortalUtils;
 import utils.TypeUtils;
-import utils.WebDriverUtils;
-
 import java.util.List;
-
-/**
- * User: sergiich
- * Date: 4/10/14
- */
 
 public class ResponsibleGamingTest extends AbstractTest{
 
@@ -132,32 +120,6 @@ public class ResponsibleGamingTest extends AbstractTest{
 		responsibleGamingPage.setRandomTimePerSession();
 		responsibleGamingPage.submitTimePerSession();
 		TypeUtils.assertTrueWithLogs(responsibleGamingPage.timePerSessionChangedSuccessfullyMessageVisible(), "successfullySetLimitMessageDisplayed");
-	}
-
-    /* 11. Set self-exlusion and verify player is logged out */
-	@Test(groups = {"regression"})
-	public void setSelfExclude(){
-        PortalUtils.registerUser(defaultUserData.getRandomUserData());
-        ResponsibleGamingPage responsibleGamingPage= (ResponsibleGamingPage) NavigationUtils.navigateToPage(ConfiguredPages.responsibleGaming);
-		SelfExcludePopup selfExcludePopup=responsibleGamingPage.navigateToSelfExclude();
-		SelfExcludeConfirmPopup selfExcludeConfirmPopup=selfExcludePopup.submitSelfExclude();
-		boolean confirmationMessageVisible=selfExcludeConfirmPopup.confirmationMessagesVisible();
-		selfExcludeConfirmPopup.clickOk();
-		try{
-			WebDriverUtils.waitForElementToDisappear(Header.LOGGED_IN_XP);
-		}catch(RuntimeException e){
-			WebDriverUtils.runtimeExceptionWithLogs("User was not logged out. Self exclusion failed.");
-		}
-		TypeUtils.assertTrueWithLogs(confirmationMessageVisible, "confirmationMessageVisible");
-	}
-
-    /* 13. Close self-exclusion popup */
-	@Test(groups = {"regression"})
-	public void cancelSelfExclusion(){
-		ResponsibleGamingPage responsibleGamingPage = (ResponsibleGamingPage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.responsibleGaming, defaultUserData.getRegisteredUserData());
-		SelfExcludePopup selfExcludePopup = responsibleGamingPage.navigateToSelfExclude();
-		selfExcludePopup.clickClose();
-		TypeUtils.assertTrueWithLogs(new AbstractPage().isLoggedIn(), "Logged in");
 	}
 
     /* 14. Several consecutive updates of deposit limits */
@@ -274,19 +236,4 @@ public class ResponsibleGamingTest extends AbstractTest{
         TypeUtils.assertTrueWithLogs(responsibleGamingPage.monthlyValidationErrorMessageVisible(),"monthlyErrorVisible");
 	}
 
-	@Test(groups = {"regression"})
-	public void setSelfExclusion(){
-        PortalUtils.registerUser(defaultUserData.getRandomUserData());
-        ResponsibleGamingPage responsibleGamingPage= (ResponsibleGamingPage) NavigationUtils.navigateToPage(ConfiguredPages.responsibleGaming);
-		SelfExcludePopup selfExcludePopup=responsibleGamingPage.navigateToSelfExclude();
-		SelfExcludeConfirmPopup selfExcludeConfirmPopup=selfExcludePopup.submitSelfExclude();
-		boolean confirmationMessageVisible=selfExcludeConfirmPopup.confirmationMessagesVisible();
-		selfExcludeConfirmPopup.clickOk();
-		try{
-			WebDriverUtils.waitForElementToDisappear(LoggedInHeader.LOGGED_IN_XP);
-		}catch(RuntimeException e){
-			WebDriverUtils.runtimeExceptionWithLogs("User was not logged out. Self exclusion failed.");
-		}
-		TypeUtils.assertTrueWithLogs(confirmationMessageVisible);
-	}
 }
