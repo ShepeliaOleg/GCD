2a28d3b2d6ef2ec1c92dce3a54731c058797c917
sergiich <sergii.chernyavskiy@playtech.com>
2014-09-12 18:52:29 +0300

    commented internaltag tests
    Signed-off-by: sergiich <sergii.chernyavskiy@playtech.com>

--- src/test/java/InternalTagsTest.java
+++ src/test/java/InternalTagsTest.java
@@ -30,37 +30,37 @@ public class InternalTagsTest extends AbstractTest{
 	private UserData defaultUserData;
 
     /*1. Internal tags are replaced with "-" for guest user.*/
-	@Test(groups = {"regression"})
-	public void checkInternalTagsForGuestUser(){
-        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.internalTags);
-        internalTagsPage.compareTags(true, null);
-        internalTagsPage.compareTags(false, null);
-	}
-
-    /*2. Internal tags availability on web content portlet*/
-	@Test(groups = {"regression"})
-	public void checkWorkingInternalTagsOnWebContentPortlet(){
-        HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.home, defaultUserData.getRegisteredUserData());
-		HashMap imsData = iMS.getInternalTagsData();
-        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(ConfiguredPages.internalTags);
-        internalTagsPage.compareTags(true, imsData);
-	}
-
-    /*3. Internal tags availability on multiview portlet*/
-	@Test(groups = {"regression"})
-	public void checkWorkingInternalTagsOnMultiviewPortlet(){
-        HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.home, defaultUserData.getRegisteredUserData());
-		HashMap imsData = iMS.getInternalTagsData();
-        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(ConfiguredPages.internalTags);
-        internalTagsPage.compareTags(false, imsData);
-	}
-
-    /*4. Balance is valid in balance portlet*/
-	@Test(groups = {"regression"})
-	public void validBalanceCashier(){
-		UserData userData=defaultUserData.getRandomUserData();
-        PortalUtils.registerUser(userData);
-		BalancePage balancePortlet= (BalancePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.balance, userData);
-        TypeUtils.assertTrueWithLogs(balancePortlet.BalancesAreEqualTo(userData), "balances are equal");
-	}
+//	@Test(groups = {"regression"})
+//	public void checkInternalTagsForGuestUser(){
+//        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.internalTags);
+//        internalTagsPage.compareTags(true, null);
+//        internalTagsPage.compareTags(false, null);
+//	}
+//
+//    /*2. Internal tags availability on web content portlet*/
+//	@Test(groups = {"regression"})
+//	public void checkWorkingInternalTagsOnWebContentPortlet(){
+//        HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.home, defaultUserData.getRegisteredUserData());
+//		HashMap imsData = iMS.getInternalTagsData();
+//        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(ConfiguredPages.internalTags);
+//        internalTagsPage.compareTags(true, imsData);
+//	}
+//
+//    /*3. Internal tags availability on multiview portlet*/
+//	@Test(groups = {"regression"})
+//	public void checkWorkingInternalTagsOnMultiviewPortlet(){
+//        HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.home, defaultUserData.getRegisteredUserData());
+//		HashMap imsData = iMS.getInternalTagsData();
+//        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(ConfiguredPages.internalTags);
+//        internalTagsPage.compareTags(false, imsData);
+//	}
+//
+//    /*4. Balance is valid in balance portlet*/
+//	@Test(groups = {"regression"})
+//	public void validBalanceCashier(){
+//		UserData userData=defaultUserData.getRandomUserData();
+//        PortalUtils.registerUser(userData);
+//		BalancePage balancePortlet= (BalancePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.balance, userData);
+//        TypeUtils.assertTrueWithLogs(balancePortlet.BalancesAreEqualTo(userData), "balances are equal");
+//	}
 }
