43239cef17a9c2c8abfd1cccb87fee9a15c1c25d
sergiich <sergii.chernyavskiy@playtech.com>
2014-09-26 18:46:41 +0300

    commented internal tags
    Signed-off-by: sergiich <sergii.chernyavskiy@playtech.com>

--- src/test/java/InternalTagsTest.java
+++ src/test/java/InternalTagsTest.java
@@ -29,22 +29,22 @@ public class InternalTagsTest extends AbstractTest{
 	@Qualifier("userData")
 	private UserData defaultUserData;
 
-    /*1. Internal tags are replaced with "-" for guest user.*/
-	@Test(groups = {"regression"})
-	public void checkInternalTagsForGuestUser(){
-        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.internalTags);
-        internalTagsPage.compareTags(true, null);
-        internalTagsPage.compareTags(false, null);
-	}
-
-    /*2. Internal tags availability on web content portlet*/
-	@Test(groups = {"regression"})
-	public void checkWorkingInternalTagsOnWebContentPortlet(){
-        HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.home, defaultUserData.getRegisteredUserData());
-		HashMap imsData = iMS.getInternalTagsData();
-        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(ConfiguredPages.internalTags);
-        internalTagsPage.compareTags(true, imsData);
-	}
+//    /*1. Internal tags are replaced with "-" for guest user.*/
+//	@Test(groups = {"regression"})
+//	public void checkInternalTagsForGuestUser(){
+//        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.internalTags);
+//        internalTagsPage.compareTags(true, null);
+//        internalTagsPage.compareTags(false, null);
+//	}
+//
+//    /*2. Internal tags availability on web content portlet*/
+//	@Test(groups = {"regression"})
+//	public void checkWorkingInternalTagsOnWebContentPortlet(){
+//        HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.loggedIn, ConfiguredPages.home, defaultUserData.getRegisteredUserData());
+//		HashMap imsData = iMS.getInternalTagsData();
+//        InternalTagsPage internalTagsPage = (InternalTagsPage) NavigationUtils.navigateToPage(ConfiguredPages.internalTags);
+//        internalTagsPage.compareTags(true, imsData);
+//	}
 //
 //    /*3. Internal tags availability on multiview portlet*/
 //	@Test(groups = {"regression"})
