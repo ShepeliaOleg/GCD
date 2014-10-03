2626a5f295bc69a5e57ec5abb3d6bb528ada4f1b
sergiich <sergii.chernyavskiy@playtech.com>
2014-10-03 16:33:38 +0300

    removed crap
    Signed-off-by: sergiich <sergii.chernyavskiy@playtech.com>

--- src/test/java/LoginLogoutConfirmationTest.java
+++ src/test/java/LoginLogoutConfirmationTest.java
@@ -68,36 +68,4 @@ public class LoginLogoutConfirmationTest extends AbstractTest {
         LoginPopup loginPopup = signedOutPopup.loginAgain();
         assertTrue(loginPopup.login(userData).isLoggedIn(), "User is logged in");
     }
-
-    /* VALIDATION */
-
-//	/*1. Username in header*/
-//	@Test(groups = {"validation"})
-//	public void usernameFieldValidationInHeader() {
-//		HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.home);
-//		homePage.loggedOutHeader().validateUsername(usernameValidationRule);
-//  	}
-//
-//	/*2. Password in header*/
-//	@Test(groups = {"validation"})
-//	public void passwordFieldValidationInHeader() {
-//		HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.home);
-//		homePage.loggedOutHeader().validatePassword(passwordValidationRule);
-//	}
-//
-//	/*3. Username in login popup*/
-//	@Test(groups = {"validation"})
-//	public void usernameFieldValidationInLoginPopup() {
-//		HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.home);
-//		LoginPopup loginPopup = homePage.navigateToLoginForm();
-//		loginPopup.validateUsername(usernameValidationRule);
-//	}
-//
-//	/*4. Password in login popup*/
-//	@Test(groups = {"validation"})
-//	public void passwordFieldValidationInLoginPopup() {
-//		HomePage homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.guest, ConfiguredPages.home);
-//		LoginPopup loginPopup = homePage.navigateToLoginForm();
-//		loginPopup.validatePassword(passwordValidationRule);
-//	}
 }
