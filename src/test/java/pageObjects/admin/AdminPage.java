6c89e7bb15924a44ae613a62d5925df7ba0b4c7f
sergiich <sergii.chernyavskiy@playtech.com>
2014-10-15 19:32:19 +0300

    admin temp fix
    Signed-off-by: sergiich <sergii.chernyavskiy@playtech.com>

--- src/test/java/pageObjects/admin/AdminPage.java
+++ src/test/java/pageObjects/admin/AdminPage.java
@@ -9,7 +9,7 @@ public class AdminPage extends AbstractPage {
     public final static String DOCKBAR_XP = "//*[@id='dockbar']";
     private final static String FIELD_USERNAME_XP = "//*[@id='_58_login']";
     private final static String FIELD_PASSWORD_XP = "//*[@id='_58_password']";
-    private final static String BUTTON_LOGIN_XP = ROOT_XP+"//*[@class='btn']";
+    private final static String BUTTON_LOGIN_XP = ROOT_XP+"//input[@class='btn']";
     private final static String BUTTON_LOGOUT_XP = "//*[@class='sign-out']";
 
     public AdminPage(){
@@ -21,17 +21,22 @@ public class AdminPage extends AbstractPage {
     }
 
     private void fillUsername(){
-        WebDriverUtils.inputTextToField(FIELD_USERNAME_XP, getAdminUserData().getAdminUsername());
+        WebDriverUtils.executeScript("document.getElementById('_58_login').value = 'sergii.chernyavskiy'");
+//        WebDriverUtils.inputTextToField(FIELD_USERNAME_XP, getAdminUserData().getAdminUsername(), 100);
     }
 
     private void fillPassword(){
-        WebDriverUtils.inputTextToField(FIELD_PASSWORD_XP, getAdminUserData().getAdminUsername());
+        WebDriverUtils.executeScript("document.getElementById('_58_password').value = '9875321Res'");
+//        WebDriverUtils.inputTextToField(FIELD_PASSWORD_XP, getAdminUserData().getAdminUsername(), 100);
     }
 
     public AbstractPage loginAdmin() {
-        fillUsername();
-        fillPassword();
-        clickLogin();
+        while(WebDriverUtils.isVisible(FIELD_USERNAME_XP, 0)) {
+            fillUsername();
+            fillPassword();
+            clickLogin();
+            WebDriverUtils.waitFor();
+        }
         return waitForAdminLogin();
     }
 
