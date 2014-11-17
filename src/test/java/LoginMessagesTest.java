08b69733bcba91a61aad5d636f8fd8e10eaeb54a
Sergiich <sergii.chernyavskiy@playtech.com>
2014-11-17 15:52:11 +0200

    uncommented tests
    Signed-off-by: Sergiich <sergii.chernyavskiy@playtech.com>

--- src/test/java/LoginMessagesTest.java
+++ src/test/java/LoginMessagesTest.java
@@ -16,7 +16,7 @@ import utils.core.DataContainer;
 public class LoginMessagesTest extends AbstractTest{
 
     /*One login message */
-	@Test(groups = {"regression", "mobile", "test"})
+	@Test(groups = {"regression", "mobile"})
 	public void loginMessage(){
         IMSUtils.setLoginMessagesCount(1);
 		UserData userData = DataContainer.getUserData().getRegisteredUserData();
@@ -24,70 +24,70 @@ public class LoginMessagesTest extends AbstractTest{
         assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0]), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
 	}
 
-//    /*No login message */
-//    @Test(groups = {"regression", "mobile", "test"})
-//    public void noLoginMessage(){
-//        IMSUtils.setLoginMessagesCount(0);
-//        try{
-//            UserData userData = DataContainer.getUserData().getRegisteredUserData();
-//            PortalUtils.loginUser(userData);
-//        }catch (Exception e){
-//            addError(e.getMessage());
-//        }finally {
-//            IMSUtils.setLoginMessagesCount(1);
-//        }
-//    }
-//
-//    /*Login messages navigation */
-//    @Test(groups = {"regression", "mobile", "test"})
-//    public void multipleLoginMessagesNavigation(){
-//        IMSUtils.setLoginMessagesCount(3);
-//        UserData userData = DataContainer.getUserData().getRegisteredUserData();
-//        WelcomePopup welcomePopup = (WelcomePopup) PortalUtils.loginUser(userData, Page.welcomePopup);
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
-//        welcomePopup.clickNext();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
-//        welcomePopup.clickNext();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[2], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[2]+"'");
-//        welcomePopup.clickPrevious();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
-//        welcomePopup.clickPrevious();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
-//    }
-//
-//    /*Login messages closing from first*/
-//    @Test(groups = {"regression", "mobile", "test"})
-//    public void multipleLoginMessagesClosingStart(){
-//        IMSUtils.setLoginMessagesCount(3);
-//        UserData userData = DataContainer.getUserData().getRegisteredUserData();
-//        WelcomePopup welcomePopup = (WelcomePopup) PortalUtils.loginUser(userData, Page.welcomePopup);
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
-//        welcomePopup.closePopup();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
-//        welcomePopup.closePopup();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[2], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[2]+"'");
-//    }
-//
-//    /*Login messages closing from last*/
-//    @Test(groups = {"regression", "mobile", "test"})
-//    public void multipleLoginMessagesClosingEnd(){
-//        IMSUtils.setLoginMessagesCount(3);
-//        UserData userData = DataContainer.getUserData().getRegisteredUserData();
-//        WelcomePopup welcomePopup = (WelcomePopup) PortalUtils.loginUser(userData, Page.welcomePopup);
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
-//        welcomePopup.clickNext();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
-//        welcomePopup.closePopup();
-//        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
-//    }
-//
-//	/*Push logout */
-//	@Test(groups = {"regression", "mobile", "test"})
-//	public void pushLogout(){
-//        UserData userData = DataContainer.getUserData().getRegisteredUserData();
-//        NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.home, userData);
-//		IMSUtils.sendPushMessage(userData, Page.logout);
-//        validateTrue(WebDriverUtils.isVisible(SignedOutPopup.TITLE_XP, 300), "User was not logged out after 180 seconds");
-//		new SignedOutPopup().close().waitForLogout();
-//	}
+    /*No login message */
+    @Test(groups = {"regression", "mobile"})
+    public void noLoginMessage(){
+        IMSUtils.setLoginMessagesCount(0);
+        try{
+            UserData userData = DataContainer.getUserData().getRegisteredUserData();
+            PortalUtils.loginUser(userData);
+        }catch (Exception e){
+            addError(e.getMessage());
+        }finally {
+            IMSUtils.setLoginMessagesCount(1);
+        }
+    }
+
+    /*Login messages navigation */
+    @Test(groups = {"regression", "mobile"})
+    public void multipleLoginMessagesNavigation(){
+        IMSUtils.setLoginMessagesCount(3);
+        UserData userData = DataContainer.getUserData().getRegisteredUserData();
+        WelcomePopup welcomePopup = (WelcomePopup) PortalUtils.loginUser(userData, Page.welcomePopup);
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
+        welcomePopup.clickNext();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
+        welcomePopup.clickNext();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[2], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[2]+"'");
+        welcomePopup.clickPrevious();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
+        welcomePopup.clickPrevious();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
+    }
+
+    /*Login messages closing from first*/
+    @Test(groups = {"regression", "mobile"})
+    public void multipleLoginMessagesClosingStart(){
+        IMSUtils.setLoginMessagesCount(3);
+        UserData userData = DataContainer.getUserData().getRegisteredUserData();
+        WelcomePopup welcomePopup = (WelcomePopup) PortalUtils.loginUser(userData, Page.welcomePopup);
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
+        welcomePopup.closePopup();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
+        welcomePopup.closePopup();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[2], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[2]+"'");
+    }
+
+    /*Login messages closing from last*/
+    @Test(groups = {"regression", "mobile"})
+    public void multipleLoginMessagesClosingEnd(){
+        IMSUtils.setLoginMessagesCount(3);
+        UserData userData = DataContainer.getUserData().getRegisteredUserData();
+        WelcomePopup welcomePopup = (WelcomePopup) PortalUtils.loginUser(userData, Page.welcomePopup);
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
+        welcomePopup.clickNext();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[1], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[1]+"'");
+        welcomePopup.closePopup();
+        assertTrue(WebDriverUtils.isVisible(IMSLoginDatabasePage.MESSAGES[0], 1), "Message correct '"+IMSLoginDatabasePage.MESSAGES[0]+"'");
+    }
+
+	/*Push logout */
+	@Test(groups = {"regression", "mobile"})
+	public void pushLogout(){
+        UserData userData = DataContainer.getUserData().getRegisteredUserData();
+        NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.home, userData);
+		IMSUtils.sendPushMessage(userData, Page.logout);
+        validateTrue(WebDriverUtils.isVisible(SignedOutPopup.TITLE_XP, 300), "User was not logged out after 180 seconds");
+		new SignedOutPopup().close().waitForLogout();
+	}
 }
