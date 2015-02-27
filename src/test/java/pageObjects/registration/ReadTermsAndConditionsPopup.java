6c26da1ee7bc34cdd6a1d0f0af7929fcea304ca2
Serhii Stepanchuk <Serhii.Stepanchuk@playtech.com>
2015-02-27 15:00:19 +0200

    TermsAndCoditionsPopup change locator

--- src/test/java/pageObjects/registration/ReadTermsAndConditionsPopup.java
+++ src/test/java/pageObjects/registration/ReadTermsAndConditionsPopup.java
@@ -4,7 +4,8 @@ import pageObjects.core.AbstractPortalPopup;
 
 public class ReadTermsAndConditionsPopup extends AbstractPortalPopup{
 
-	public static final String ROOT_XP = AbstractPortalPopup.ROOT_XP + "//strong[contains(text(),'TERMS AND CONDITIONS')]";
+//    public static final String ROOT_XP = AbstractPortalPopup.ROOT_XP + "//strong[contains(text(),'TERMS AND CONDITIONS')]";
+    public static final String ROOT_XP = AbstractPortalPopup.ROOT_XP + "//*[@class='popup-modal__title'][contains(text(), 'Terms & Conditions')]";
 
 	public ReadTermsAndConditionsPopup(){
 		super(new String[]{ROOT_XP});
