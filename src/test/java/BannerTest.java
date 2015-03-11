c781e704ac0905482f40edd4cfa68096028e3cb6
Vadym Feshchenko <Vadym.Feshchenko@playtech.com>
2015-03-11 17:49:46 +0200

    BannerTest add BUG skip all tests

--- src/test/java/BannerTest.java
+++ src/test/java/BannerTest.java
@@ -14,55 +14,59 @@ public class BannerTest extends AbstractTest{
 
 	/*POSITIVE*/
 
-	/* 1. Portlet is available */
-	@Test(groups = {"smoke"})
-	public void portletIsDisplayed() {
+    /* 1. Portlet is available */
+    @Test(groups = {"smoke"})
+    public void portletIsDisplayed() {
         try{
             BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerImage);
         }catch (Exception e){
             skipTest();
         }
-	}
+    }
 
     /* 2. Banner shows image */
     @Test(groups = {"regression"})
     public void bannerImage() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerImage);
         List<BannerSlideType> slidesTypes = bannerPage.getSlidesTypes();
-        validateEquals(3, slidesTypes.size(), "Count of slides");
+        validateEquals(1, slidesTypes.size(), "Number of slides");
         assertEquals(BannerSlideType.image, slidesTypes.get(0), "Slide type");
-        assertTrue(bannerPage.slideIsDisplayed(1, 3), "Slide with Image is not displayed.");
+        assertTrue(bannerPage.slideIsDisplayed(1), "Slide with Image is not displayed.");
     }
 
     /* 3. Banner shows HTML */
     @Test(groups = {"regression"})
     public void bannerHtml() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerHtml);
         List<BannerSlideType> slidesTypes = bannerPage.getSlidesTypes();
-        validateEquals(3, slidesTypes.size(), "Count of slides");
+        validateEquals(1, slidesTypes.size(), "Number of slides");
         assertEquals(BannerSlideType.html, slidesTypes.get(0), "Slide type");
-        assertTrue(bannerPage.slideIsDisplayed(1, 3), "Slide with HTML is not displayed.");
+        assertTrue(bannerPage.slideIsDisplayed(1), "Slide with HTML is not displayed.");
     }
 
     /* 4. Banner shows WebContent */
     @Test(groups = {"regression"})
     public void bannerWebContent() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerWebContent);
         List<BannerSlideType> slidesTypes = bannerPage.getSlidesTypes();
-        validateEquals(3, slidesTypes.size(), "Number of slides");
+        validateEquals(1, slidesTypes.size(), "Number of slides");
         assertEquals(BannerSlideType.webContent, slidesTypes.get(0), "Slide type");
-        assertTrue(bannerPage.slideIsDisplayed(1, 3), "Slide with WebContent is not displayed.");
+        assertTrue(bannerPage.slideIsDisplayed(1), "Slide with WebContent is not displayed.");
     }
 
     /* 5. Banner shows mixed content */
     @Test(groups = {"regression"})
     public void bannerMixed() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerMixed);
         List<BannerSlideType> slidesTypes = bannerPage.getSlidesTypes();
-        validateEquals(7, slidesTypes.size(), "Number of slides");
-        assertEquals(BannerSlideType.html, slidesTypes.get(0),      "Second slide not a HTML");
-        assertEquals(BannerSlideType.webContent, slidesTypes.get(1),"Third slide not a WEBCONTENT");
-        assertEquals(BannerSlideType.image, slidesTypes.get(2),     "First slide not a IMAGE");
+        validateEquals(3, slidesTypes.size(), "Number of slides");
+        assertEquals(BannerSlideType.image, slidesTypes.get(0),     "First slide");
+        assertEquals(BannerSlideType.html, slidesTypes.get(1),      "Second slide");
+        assertEquals(BannerSlideType.webContent, slidesTypes.get(2),"Third slide");
         assertTrue(bannerPage.slideIsDisplayed(1, 10), "First slide with image was not displayed.");
         assertTrue(bannerPage.slideIsDisplayed(2, 10), "Second slide with HTML was not displayed");
         assertTrue(bannerPage.slideIsDisplayed(3, 10), "Third slide with WebContent was not displayed.");
@@ -71,6 +75,7 @@ public class BannerTest extends AbstractTest{
     /* 6. Navigation – buttons*/
     @Test(groups = {"regression"})
     public void bannerNavigationButtons() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerNavigationButtons);
         assertEquals(BannerNavigationType.buttons, bannerPage.getNavigationType(), "Navigation type");
         bannerPage.showSlide(1);
@@ -84,6 +89,7 @@ public class BannerTest extends AbstractTest{
     /* 7. Navigation – arrows*/
     @Test(groups = {"regression"})
     public void bannerNavigationArrows() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerNavigationArrows);
         assertEquals(BannerNavigationType.arrows, bannerPage.getNavigationType(), "Navigation type");
         bannerPage.showSlide(1);
@@ -101,11 +107,13 @@ public class BannerTest extends AbstractTest{
         bannerPage.showSlide(1);
         assertTrue(bannerPage.slideIsDisplayed(1), "First slide was not displayed after clicking previous arrow.");
         assertTrue(bannerPage.arrowsDisplayed(),   "Only Next arrow should be displayed on first slide.");
+
     }
 
     /* 8. Navigation - arrows and bullets*/
     @Test(groups = {"regression"})
     public void bannerNavigationArrowsBullets() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerNavigationArrowsBullets);
         assertTrue(bannerPage.slideIsDisplayed(1), "First slide is not displayed.");
         assertEquals(BannerNavigationType.arrowsAndBullets, bannerPage.getNavigationType(), "Navigation type");
@@ -138,6 +146,7 @@ public class BannerTest extends AbstractTest{
     /* 9. Navigation – bullets*/
     @Test(groups = {"regression"})
     public void bannerNavigationBullets() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerNavigationBullets);
         assertTrue(bannerPage.slideIsDisplayed(1), "First slide is not displayed.");
         assertEquals(BannerNavigationType.bullets, bannerPage.getNavigationType(), "Navigation type");
@@ -152,14 +161,16 @@ public class BannerTest extends AbstractTest{
     /* 10. Navigation – none*/
     @Test(groups = {"regression"})
     public void bannerNavigationNone() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerImage);
-        assertTrue(bannerPage.slideIsDisplayed(1, 3), "First slide is not displayed.");
+        assertTrue(bannerPage.slideIsDisplayed(1), "First slide is not displayed.");
         assertEquals(BannerNavigationType.none, bannerPage.getNavigationType(), "Navigation type");
     }
 
     /* 11. Banner as link*/
     @Test(groups = {"regression"})
     public void bannerLink() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerLink);
         assertTrue(bannerPage.slideIsDisplayed(1), "First slide is not displayed.");
         bannerPage.clickSlide(1);
@@ -173,6 +184,7 @@ public class BannerTest extends AbstractTest{
     /* 12. Include banner in rotation*/
     @Test(groups = {"regression"})
     public void bannerInRotation() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.bannerInRotation);
         List<BannerSlideType> slidesTypes = bannerPage.getSlidesTypes();
         validateEquals(2, slidesTypes.size(), "Number of slides");
@@ -180,11 +192,12 @@ public class BannerTest extends AbstractTest{
         assertEquals(BannerSlideType.webContent, slidesTypes.get(1), "Second slide");
         assertTrue(bannerPage.slideIsDisplayed(1), "First slide is displayed");
         assertTrue(bannerPage.slideIsDisplayed(2), "Second slide is displayed");
-
     }
+
     /* 13. Time to display*/
     @Test(groups = {"regression"})
     public void banner5seconds() {
+        skipTestWithIssues("D-19983");
         BannerPage bannerPage = (BannerPage) NavigationUtils.navigateToPage(ConfiguredPages.banner5seconds);
         int slidesNumber = bannerPage.getSlidesCount();
         validateEquals(3, slidesNumber, "Number of slides");
