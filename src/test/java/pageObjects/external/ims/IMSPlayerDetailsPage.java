8fc40ee15987d0dc71638d6c6e7de0c9b3bcc058
Serhii Stepanchuk <Serhii.Stepanchuk@playtech.com>
2015-03-31 12:02:22 +0300

    Fixed xpath to device detection field on ims

--- src/test/java/pageObjects/external/ims/IMSPlayerDetailsPage.java
+++ src/test/java/pageObjects/external/ims/IMSPlayerDetailsPage.java
@@ -60,7 +60,7 @@ public class IMSPlayerDetailsPage extends AbstractServerPage {
 	private static final String FIELD_VERIFICATION_QUESTION=				"//*[@id='verificationquestion']";
 	private static final String FIELD_VERIFICATION_ANSWER=					"//*[@id='verificationanswer']";
     private static final String FIELD_CLIENT_TYPE=                          "//*[preceding-sibling::*[contains(text(), 'Sign up client type:')]][1]";
-    private static final String FIELD_LAST_LOGIN_DEVICE_TYPE =              "//*[@id='ssec_supinfo' and contains(., 'Last login deviceId type:')]/td[4]";
+    private static final String FIELD_LAST_LOGIN_DEVICE_TYPE =              "//*[@id='ssec_supinfo' and contains(., 'Last login device type:')]/td[4]";
     private static final String FIELD_TOTAL_BONUS_AMOUNT_XP =               "//*[@id='ssec_bonusinfo']//tr[last()]/td[3]";
 	private static final String BUTTON_KILL_PLAYER=							"//*[@id='killplayer']";
 	private static final String BUTTON_ADD_BONUS= 							"//*[@value='Add bonus']";
@@ -485,7 +485,7 @@ public class IMSPlayerDetailsPage extends AbstractServerPage {
 
 
     public String getDeviceType() {
-        WebDriverUtils.waitFor(500);
+        WebDriverUtils.waitForElement(WebDriverFactory.getServerDriver(), FIELD_LAST_LOGIN_DEVICE_TYPE);
         return WebDriverUtils.getElementText(WebDriverFactory.getServerDriver(), FIELD_LAST_LOGIN_DEVICE_TYPE);
     }
 
