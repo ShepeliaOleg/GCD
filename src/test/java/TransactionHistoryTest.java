b0306ce4eda244731fbd7a8d7fcc65f1abf76abc
Vadym Feshchenko <Vadym.Feshchenko@playtech.com>
2015-04-24 09:59:50 +0300

    TransactionHistory: add one new test method

--- src/test/java/TransactionHistoryTest.java
+++ src/test/java/TransactionHistoryTest.java
@@ -1,9 +1,17 @@
 import enums.ConfiguredPages;
+import enums.Page;
 import enums.PlayerCondition;
 import org.testng.annotations.Test;
+import pageObjects.HomePage;
 import pageObjects.account.TransactionHistoryPage;
+import utils.IMSUtils;
 import utils.NavigationUtils;
 import utils.core.AbstractTest;
+import utils.core.DataContainer;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 /**
@@ -11,28 +19,42 @@ import utils.core.AbstractTest;
  */
 public class TransactionHistoryTest extends AbstractTest {
 
+    private static final String BONUS_AMOUNT = "0.50";
+    private TransactionHistoryPage trHistory;
+    private DateFormat dateFormat;
+    private Date currentTime;
+    private HomePage homePage;
+    private String oldBalance;
+    private String currencySymbol = " ";
+    private String curDate;
+
     @Test(groups = {"regression", "P3", "COR-473", "TransactionHistory"})
     public void shouldSeeListOfGroups(){
 
-        TransactionHistoryPage trHistory = (TransactionHistoryPage) NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.transactionHistory);
+        trHistory = (TransactionHistoryPage) NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.transactionHistory);
 
         assertEquals("All", trHistory.getGroupText(TransactionHistoryPage.ALL_OPT), "First tab(group) capture was not as expected");
         assertEquals("Games", trHistory.getGroupText(TransactionHistoryPage.GAME_OPT), "Second tab(group) capture was not as expected");
         assertEquals("General", trHistory.getGroupText(TransactionHistoryPage.GENERAL_OPT), "Third tab(group) capture was not as expected");
     }
 
-    @Test
-    public void playerShouldHasGeneralTransactions(){
-        TransactionHistoryPage trHistory = (TransactionHistoryPage) NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.transactionHistory);
+    @Test(groups = {"regression", "P1", "COR-474", "TransactionHistory"})
+    public void shouldSeeLastGeneralTransactions(){
+
+        homePage = (HomePage) NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.home);
+        prepareBonus();
+        trHistory = (TransactionHistoryPage) NavigationUtils.navigateToPage(PlayerCondition.player, ConfiguredPages.transactionHistory);
 
         trHistory.selectPeriod(TransactionHistoryPage.ONE_DAY);
         trHistory.clickGroups(TransactionHistoryPage.GENERAL_OPT);
         trHistory.clickViewHistoryBtn();
 
-        System.out.println(trHistory.getTransactionCount());
         //ToDo check
-        assertTrue(trHistory.getTransactionCount() > 0, "");
-        System.out.println("CHECK FINISHED");
+        //assertTrue(trHistory.getEntryTime(0).contains(curDate), "Transaction History entry time was not as expected!");
+        assertTrue(trHistory.isEntryTimeVisible(0, currentTime), "Transaction History entry time was not as expected!");
+        assertEquals("Add bonus", trHistory.getEntryType(0), "Transaction History entry type was not as expected!");
+        assertEquals(BONUS_AMOUNT + currencySymbol, trHistory.getEntryAmount(0), "Transaction History entry amount was not as expected!");
+        assertEquals(homePage.getBalanceAmount() + currencySymbol, trHistory.getEntryBalance(0), "Transaction History entry amount was not as expected!");
     }
 
     @Test
@@ -46,7 +68,6 @@ public class TransactionHistoryTest extends AbstractTest {
         System.out.println(trHistory.getTransactionCount());
         //ToDo check
         assertTrue(trHistory.getTransactionCount() > 0, "");
-        System.out.println("CHECK FINISHED");
     }
 
     @Test
@@ -60,6 +81,16 @@ public class TransactionHistoryTest extends AbstractTest {
         System.out.println(trHistory.getTransactionCount());
         //ToDo check
         assertTrue(trHistory.getTransactionCount() > 0, "");
-        System.out.println("CHECK FINISHED");
+    }
+
+    private void prepareBonus(){
+        oldBalance = homePage.getBalanceAmount();
+        currencySymbol += DataContainer.getUserData().getCurrencySign();
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        currentTime = new Date();
+        curDate = dateFormat.format(currentTime);
+        curDate = curDate.substring(0, curDate.length() - 1);
+        //System.out.println(curDate);
+        IMSUtils.sendPushMessage(DataContainer.getUserData(), BONUS_AMOUNT, Page.okBonus);
     }
 }
--- src/test/java/pageObjects/account/TransactionHistoryPage.java
+++ src/test/java/pageObjects/account/TransactionHistoryPage.java
@@ -3,6 +3,10 @@ package pageObjects.account;
 import pageObjects.core.AbstractPortalPage;
 import utils.WebDriverUtils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TransactionHistoryPage extends AbstractPortalPage {
 
     private static final String ROOT_XP = "//*[contains(@class,'portlet-mobile-cashier')]";
@@ -10,6 +14,7 @@ public class TransactionHistoryPage extends AbstractPortalPage {
     private static final String PERIOD_SELECT_XP = "//*[contains(@class,'fn-transaction-select')]/option[@value='";
     private static final String VIEW_HISTORY_BTN_XP = "//*[contains(@class,'portlet__actions')]/button[@class='btn']";
     private static final String TRANSACTION_LIST = "//*[contains(@class,'fn-transaction-list')]/li";
+    private static final String ENTRY_TYPE = TRANSACTION_LIST + "[position()=last()-0]/div[1]/span[contains(@class, 'val_type_type')]";
 
     public static final String ONE_DAY = "1";
     public static final String THREE_DAYS = "3";
@@ -43,6 +48,33 @@ public class TransactionHistoryPage extends AbstractPortalPage {
 
     public int getTransactionCount(){
         return WebDriverUtils.getXpathCount(TRANSACTION_LIST);
+    }
+
+    public String getEntryType(int index){
+        return WebDriverUtils.getElementText(TRANSACTION_LIST + "[position()=last()-" + index + "]/div[1]/span[contains(@class, 'val_type_type')]");
+    }
+
+    public String getEntryAmount(int index) {
+        return WebDriverUtils.getElementText(TRANSACTION_LIST + "[position()=last()-" + index + "]/div[1]/span[contains(@class, 'val_type_amount')]");
+    }
+
+    public String getEntryBalance(int index) {
+        return WebDriverUtils.getElementText(TRANSACTION_LIST + "[position()=last()-" + index + "]/div[1]/span[contains(@class, 'val_type_balance')]");
+    }
+
+    public String getEntryTime(int index){
+        return WebDriverUtils.getElementText(TRANSACTION_LIST + "[position()=last()-" + index + "]/div[1]/time");
+    }
+
+    public boolean isEntryTimeVisible(int index, Date date){
+        Date incDate = new Date(date.getTime());
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        incDate.setMinutes(incDate.getMinutes() + 1);
+        String curentMinutes = TRANSACTION_LIST + "[position()=last()-" + index + "]/div[1]/time[contains(text(), '"+dateFormat.format(date)+"')]";
+        dateFormat.format(incDate);
+        String minutesPlusOne = TRANSACTION_LIST + "[position()=last()-" + index + "]/div[1]/time[contains(text(), '"+dateFormat.format(incDate)+"')]";
+        return WebDriverUtils.isVisible(curentMinutes + " | " + minutesPlusOne);
     }
 }
 
